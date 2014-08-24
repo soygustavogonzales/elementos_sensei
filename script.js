bf6203d1cfecd0da9d995659fd71d5f054b759c3
Gustavo Gonzales <gustavo.gonda@gmail.com>
2014-08-24 00:27:48 -0500

    mejoras en el drag and drop/mas agilidad que en la rama master

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
         paper.rect(0,0,1200,560).attr({
         	fill: 'rgba(0,0,0,0)'
         });
-  var set = paper.set();
+  	var set = paper.set();
 
 
 		var $btnCrearRect = $('.rectangle'),
@@ -12,9 +12,11 @@
 		$ancho = $('.ancho'),
 		$alto = $('.alto'),
 		$eliminar = $('.eliminar'),
+		$btnCrearAmorfo =$('.amorfo'),
 		svg_ = document.getElementsByTagName('svg')[0]
 		;
 
+    /*
 		svg_.onclick = function(e){
 			console.log(e.x + " , "+e.y)
 		}
@@ -27,14 +29,14 @@
 				elActual.transform("t"+e.x+","+e.y+"r"+deegre)
 			}
 		}
-		
-    /*
-        cuadrado = paper.rect(10, 15, 64, 64, 4).attr({fill: 'pink'}),
-        rectangulo = paper.rect(30, 35, 100, 64).attr({fill: 'pink'}),
-        circulo = paper.circle(150, 48, 32).attr({fill: 'lightblue'});
     */
+		
+    cuadrado = paper.rect(10, 15, 64, 64, 4).attr({fill: 'pink'}),
+    rectangulo = paper.rect(30, 35, 100, 64).attr({fill: 'pink'}),
+    circulo = paper.circle(150, 48, 32).attr({fill: 'lightblue'});
     
    /*
+   */
     set.push(cuadrado,circulo,rectangulo)
        .draggable();
 
@@ -71,7 +73,6 @@
 	    			set.drag(move, start, end);
 	    	}
 	    	watchElements();
-   */
 	var btnMoverPizarra = $('#change');
 	var pizarra = $('.pizarra')
 
@@ -150,9 +151,9 @@
     		cursor:"pointer"
     	})
     });
-			 set.push(newCuadrado)  
-    //.draggable(); 
-	   // watchElements();
+		 set.push(newCuadrado)  
+    	.draggable(); 
+	    watchElements();
 		}
 
 		function eliminarElemento (){
@@ -160,7 +161,143 @@
 			paper.clear()
 		}
 
+		$btnCrearAmorfo.click(function(event){
+			var set2 = paper.set()
+			var amorfo = paper.path("M10,80L60,110L60,60L480,160Z")
+			.attr({
+				fill:"#fff",
+				stroke:"white",
+				'stroke-width':0
+			})
+			var ccx = 0, ccy = 0;
+			if(this._&&this._['bbox']&&this._['bbox']['cx']&&this._['bbox']['cy']){
+				ccx = this._['bbox']['cx'];ccy = this._['bbox']['cy'];
+			}else{
+				console.log("...");
+			}
+			paper.circle(ccx,ccy,3)
+			.attr({
+				fill:"yellow",
+				stroke:"transparent"
+			})
+			var onend = function(e,y){
+				console.log("end")
+				console.log(e)
+				//this.transform("t"+(e.x)+","+(e.y))
+						var ccx = 0, ccy = 0;
+						if(this._&&this._['bbox']&&this._['bbox']['cx']&&this._['bbox']['cy'])
+							ccx = this._['bbox']['cx'];ccy = this._['bbox']['cy'];
+						paper.circle(ccx,ccy,3)
+						.attr({
+							fill:"white",
+							stroke:"transparent"
+						})
+			}
+			var onstart = function(x,y){
+				console.log("start")
+				//console.log(x+" : "+y)
+				this.attr({
+					fill:"blue"
+				})
+				/*
+						var ccx = 0, ccy = 0;
+						if(this._&&this._['bbox']&&this._['bbox']['cx']&&this._['bbox']['cy'])
+							ccx = this._['bbox']['cx'];ccy = this._['bbox']['cy'];
+						paper.circle(ccx,ccy,3)
+						.attr({
+							fill:"white",
+							stroke:"transparent"
+						})
+				*/
+				//this.transform("t"+x+","+y)
+			}
+			var onmove = function(dx, dy, mx, my, ev){
+				console.log("onmove")
+				console.log(this)
+				//console.log(ev)
+				//console.log(dx+","+ dy+","+ mx+","+ my)
+				    var b = this.getABox(); // Raphael's getBBox() on steroids
+				   	console.log(b);
+            var px = mx - b.offset.left,
+                py = my - b.offset.top,
+                x = this.ox + dx,
+                y = this.oy + dy,
+                r = this.is('circle') ? b.width / 2 : 0;
+            
+            // nice touch that helps you keep draggable elements within the canvas area
+            /*
+            var x = Math.min(
+                        Math.max(0 + this.margin + (this.is('circle') ? r : 0), x),
+                        this.paper.width - (this.is('circle') ? r : b.width) - this.margin),
+                y = Math.min(
+                        Math.max(0 + this.margin + (this.is('circle') ? r : 0), y),
+                        this.paper.height - (this.is('circle') ? r : b.height) - this.margin);
+            */
+             //console.log(px+" : "+py)
+            // work-smart, applies to circles and non-circles        
+            var pos = { x: x, y: y, cx: x, cy: y };
+            //console.log(pos)
+						//this.transform("t"+mx+","+my)
+						var ccx = 0, ccy = 0;
+						if(this._&&this._['bbox']&&this._['bbox']['cx']&&this._['bbox']['cy'])
+							ccx = this._['bbox']['cx'];ccy = this._['bbox']['cy'];
+						/*
+						paper.circle(ccx,ccy,3)
+						.attr({
+							fill:"white",
+							stroke:"transparent"
+						})
+						*/
+						/*
+						var ccx2 = 0, ccy2 = 0;
+						if(this._&&this._['bbox']&&this._['bbox']['x']&&this._['bbox']['y'])
+							ccx2 = this._['bbox']['x'];ccy2 = this._['bbox']['y'];
+						//console.log(ccx2)
+						paper.circle(ccx2,ccy2,3)
+						.attr({
+							fill:"green",
+							stroke:"transparent"
+						})
+						var ddx = 0, ddy = 0;
+						if(this._&&this._['dx']&&this._['dy'])
+							ddx = this._['bbox']['x2'];ddy = this._['bbox']['y2'];
+						//console.log(ddx)
+						paper.circle(ddx,ddy,3)
+						.attr({
+							fill:"blue",
+							stroke:"transparent"
+						})
+						*/
+						var ddx_ = 0, ddy_ = 0;
+						if(this._&&this._['dx']&&this._['dy'])
+							ddx_ = this._['dx'];ddy_ = this._['dy'];
+						//console.log(ddx_)
+						/*
+						paper.circle(ddx_,ddy_,3)
+						.attr({
+							fill:"red",
+							stroke:"transparent"
+						})
+						*/
 
+						var a = (ccx - ddx_), b = (ccy - ddy_);
+						var m = (ccx - a), n = (ccy - b);
+						/*
+						paper.circle(m,n,3)
+						.attr({
+							fill:"yellow",
+							stroke:"transparent"
+						})
+						*/
+
+						console.log(m+" : "+n)
+						this.transform("t"+(mx - a)+","+(my - b))
+
+			}
+			set2.push(amorfo).drag(onmove,onstart,onend)
+			//console.log(amorfo)
+	    watchElements();
+		})
 		
 		$btnCrearRect.click(function(event) {
 			var ancho = parseInt($('.ancho').val()),
